9fc8bc373431f8315f714bf27c3452cc9d1c6bd3
ibrahim <ibrahimsaad6677@gmail.com>
2024-05-02 03:03:54 +0300

    try 1

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+import datetime
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, DATETIME
-from models import storage_type
 
 Base = declarative_base()
-
 
 class BaseModel:
     """A base class for all hbnb models
@@ -23,30 +21,23 @@ class BaseModel:
                 unique=True)
     created_at = Column(DATETIME,
                         nullable=False,
-                        default=datetime.utcnow())
+                        default=datetime.datetime.utcnow)
     updated_at = Column(DATETIME,
                         nullable=False,
-                        default=datetime.utcnow())
+                        default=datetime.datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.datetime.utcnow()
+            self.updated_at = datetime.datetime.utcnow()
         else:
             for k in kwargs:
                 if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
+                    setattr(self, k, datetime.datetime.fromisoformat(kwargs[k]))
                 elif k != '__class__':
                     setattr(self, k, kwargs[k])
-            if storage_type == 'db':
-                if not hasattr(kwargs, 'id'):
-                    setattr(self, 'id', str(uuid.uuid4()))
-                if not hasattr(kwargs, 'created_at'):
-                    setattr(self, 'created_at', datetime.now())
-                if not hasattr(kwargs, 'updated_at'):
-                    setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -56,7 +47,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.utcnow()
         storage.new(self)
         storage.save()
 
